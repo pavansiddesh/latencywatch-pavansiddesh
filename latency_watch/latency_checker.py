9b261054cd31f662e772d0d3275eda68b482617f
pavansiddesh <158774038+pavansiddesh@users.noreply.github.com>
2026-03-04 23:04:26 +0530

    Update and rename latency_checker.py to profiler.py

--- latency_watch/latency_checker.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def check_latency(host="google.com"):
-    result = subprocess.run(["ping", "-c", "4", host], capture_output=True, text=True)
-    print(result.stdout)
-
-if __name__ == "__main__":
-    check_latency()
